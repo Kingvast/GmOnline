eebc5f4f38e527cedefdc2649619cade58f0fd89
Kingvast <kingvast.wh@gmail.com>
2018-09-30 00:00:19 +0800

    机构课程列表

--- src/apps/organization/views.py
+++ src/apps/organization/views.py
@@ -111,3 +111,25 @@ class OrgHomeView(View):
             'current_page': current_page,
             'has_fav': has_fav
         })
+
+
+class OrgCourseView(View):
+    """
+    机构课程列表页
+    """
+
+    def get(self, request, org_id):
+        current_page = 'course'
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        has_fav = False
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user,
+                                           fav_id=course_org.id, fav_type=2):
+                has_fav = True
+        all_courses = course_org.course_set.all()
+        return render(request, 'org-detail-course.html', {
+            'all_courses': all_courses,
+            'course_org': course_org,
+            'current_page': current_page,
+            'has_fav': has_fav
+        })
