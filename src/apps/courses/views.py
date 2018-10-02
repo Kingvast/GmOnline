16ff03bb6021e9d29d360d86f34d64256c6f80f9
Kingvast <kingvast.wh@gmail.com>
2018-10-02 17:11:37 +0800

    k课程详情功能完成

--- src/apps/courses/views.py
+++ src/apps/courses/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.db.models import Q
 from .models import Course
+from operation.models import UserFavorite, CourseComments, UserCourse
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -51,7 +52,43 @@ class CourseListView(View):
 
 
 class CourseDetailView(View):
-    pass
+    """
+    课程详情页
+    """
+
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+
+        # 增加课程点击数
+        course.click_nums += 1
+        course.save()
+
+        # 是否收藏课程
+        has_fav_course = False
+        # 是否收藏机构
+        has_fav_org = False
+
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.id,
+                                           fav_type=1):
+                has_fav_course = True
+
+            if UserFavorite.objects.filter(user=request.user,
+                                           fav_id=course.course_org.id,
+                                           fav_type=2):
+                has_fav_org = True
+
+        tag = course.tag
+        if tag:
+            relate_course = Course.objects.filter(tag=tag)[:1]
+        else:
+            relate_course = []
+        return render(request, 'course-detail.html', {
+            'course': course,
+            'relate_course': relate_course,
+            'has_fav_course': has_fav_course,
+            'has_fav_org': has_fav_org
+        })
 
 
 class CourseInfoView(View):
