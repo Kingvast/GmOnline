a8c29760d5fd6d87e0cd8937c6756a1b9a5ea536
Kingvast <kingvast.wh@gmail.com>
2018-10-05 09:54:31 +0800

    暂时完成

--- src/apps/organization/urls.py
+++ src/apps/organization/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, \
-    OrgDescView, OrgTeacherView, AddFavView
+    OrgDescView, OrgTeacherView, AddFavView, TeacherListView, TeacherDetailView
 
 urlpatterns = [
+    # 课程机构页
     url(r'^list/$', OrgView.as_view(), name='org_list'),
     url(r'^add_ask/$', AddUserAskView.as_view(), name='add_ask'),
     url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name="org_home"),
@@ -13,5 +14,13 @@ urlpatterns = [
     url(r'^org_teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(),
         name='org_teacher'),
 
+    # 机构收藏
     url(r'^add_fav/$', AddFavView.as_view(), name='add_fav'),
+
+    # 讲师列表页
+    url(r'^teacher/list/$', TeacherListView.as_view(), name='teacher_list'),
+
+    # 讲师详情页
+    url(r'^teacher/detail/(?P<teacher_id>\d+)/$', TeacherDetailView.as_view(),
+        name='teacher_detail'),
 ]
