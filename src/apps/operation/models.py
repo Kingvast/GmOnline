286a2a6d03eb23ffc44c2a66a698f4a9d4bccde4
Kingvast <kingvast.wh@gmail.com>
2018-10-06 21:53:41 +0800

    operation model 完善

--- src/apps/operation/models.py
+++ src/apps/operation/models.py
@@ -11,6 +11,9 @@ from courses.models import Course
 
 # Create your models here.
 class UserAsk(models.Model):
+    """
+    用户我要学习
+    """
     name = models.CharField(max_length=20, verbose_name=u'姓名')
     mobile = models.CharField(max_length=11, verbose_name=u'手机')
     course_name = models.CharField(max_length=50, verbose_name=u'课程名')
@@ -19,6 +22,9 @@ class UserAsk(models.Model):
     class Meta:
         verbose_name = u'用户咨询'
         verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return '用户: {0} 手机号: {1}'.format(self.name, self.mobile)
 
 
 class CourseComments(models.Model):
@@ -34,8 +40,14 @@ class CourseComments(models.Model):
         verbose_name = u'课程评论'
         verbose_name_plural = verbose_name
 
+    def __unicode__(self):
+        return '用户({0})对于《{1}》 评论 :'.format(self.user, self.course)
+
 
 class UserFavorite(models.Model):
+    """
+    用户收藏（课程，机构，讲师）
+    """
     user = models.ForeignKey(UserProfile, verbose_name=u'用户')
     fav_id = models.IntegerField(default=0, verbose_name=u'数据id')
     fav_type = models.IntegerField(
@@ -48,8 +60,14 @@ class UserFavorite(models.Model):
         verbose_name = u'用户收藏'
         verbose_name_plural = verbose_name
 
+    def __unicode__(self):
+        return '用户({0})收藏了{1} '.format(self.user, self.fav_type)
+
 
 class UserMessage(models.Model):
+    """
+    用户消息
+    """
     user = models.IntegerField(default=0, verbose_name=u'接收用户')
     message = models.CharField(max_length=500, verbose_name=u'消息内容')
     has_read = models.BooleanField(default=False, verbose_name=u'是否已读')
@@ -59,8 +77,14 @@ class UserMessage(models.Model):
         verbose_name = u'用户消息'
         verbose_name_plural = verbose_name
 
+    def __unicode__(self):
+        return '用户({0})接收了{1} '.format(self.user, self.message)
+
 
 class UserCourse(models.Model):
+    """
+    用户课程
+    """
     user = models.ForeignKey(UserProfile, verbose_name=u'用户')
     course = models.ForeignKey(Course, verbose_name=u'课程')
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
@@ -68,3 +92,6 @@ class UserCourse(models.Model):
     class Meta:
         verbose_name = u'用户课程'
         verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return '用户({0})学习了{1} '.format(self.user, self.course)
